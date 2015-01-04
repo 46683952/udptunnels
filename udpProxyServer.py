d792b2884c2f1324b0cb06d1c21799fdccb5d3d0
smerchan <smerchan@yahoo.com>
2015-01-04 00:49:50 +0000

    Added copyright note

--- udpProxyServer.py
+++ udpProxyServer.py
@@ -1,3 +1,22 @@
+#! /usr/bin/env python
+
+#                                                                         
+# udpProxyServer.py 
+#  
+# Decription: A simple Proxy Server tunneling over UDP with tun device 
+#
+# Copyright (C) 2015  Sameer Merchant <smerchan@yahoo.com>
+#                                                                         
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the 
+# Free Software Foundation; either version 2, or (at your option) any  
+# later version.                                                      
+#                                                                      
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of         
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+# General Public License for more details.                         
+#
 import sys
 import socket
 import select
